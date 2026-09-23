d531465a09447af7917c80b50d0efdf3fbd96933
maksim.meshcheryakov <xyphrodite@gmail.com>
2026-09-23 10:53:55 +0300

    fix(android): remove SetNavigationBarDividerColor (no such API)

--- src/TerminalV.Mobile/Platforms/Android/MainActivity.cs
+++ src/TerminalV.Mobile/Platforms/Android/MainActivity.cs
@@ -30,7 +30,6 @@ public class MainActivity : MauiAppCompatActivity
                 Window.Attributes!.LayoutInDisplayCutoutMode = LayoutInDisplayCutoutMode.ShortEdges;
                 Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
                 Window.SetNavigationBarColor(Android.Graphics.Color.Transparent);
-                Window.SetNavigationBarDividerColor(Android.Graphics.Color.Transparent);
             }
             var controller = Window?.InsetsController;
             if (controller != null)
